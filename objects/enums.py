77658e68e95d2374b9f0b591b46d6921297d9655
Benoit VILLAIN <benoit.villain@yeanpi.fr>
2017-10-15 18:48:57 +0200

    Adding enums file With Colors Enum

---
+++ objects/enums.py
@@ -0,0 +1,14 @@
+"""
+Title: colors File
+Desc: Colors enumeration
+Creation Date: 15/10/17
+LastMod Date: 15/10/17
+TODO:
+*
+"""
+
+from enum import Enum
+
+class Colors(Enum):
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
