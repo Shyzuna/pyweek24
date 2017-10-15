c2a30f368804dcb03456189d056cb19162d6024e
Kévin Thek <kev.thek@gmail.com>
2017-10-15 18:26:08 +0200

    Ajout settings et map manager

---
+++ modules/mapManager.py
@@ -0,0 +1,20 @@
+
+
+
+class MapManager:
+    def __init__(self, ):
+        '''
+        Constructor
+
+        '''
+
+    def load(self, path):
+        '''
+
+        :param path: path of map
+        :return:
+        '''
+
+
+
+
